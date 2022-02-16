4cf5cdde521c1fafc461b28c59809bb1f43e0575
VolodymyrNehir <volodymyrnehir@gmail.com>
2022-02-16 21:25:34 +0200

    Initial commit

---
+++ lesson1/homeWork/app.js
@@ -0,0 +1,75 @@
+const  path = require("path");
+const fs = require("fs");
+
+
+
+fs.mkdir(path.join(__dirname,"main","online" ), {recursive: true},(err => {
+    if (err){
+        console.log(err)
+    }
+}));
+fs.mkdir(path.join(__dirname,"main","inPersons"),(err)=>{
+    console.log(err)
+})
+// create directory
+
+
+let  onlineUser= [{name: "Andrii", age: 22, city: "Lviv" }];
+let  inPersonsUser= [{name: "Oleg", age: 24, city: "Lviv" }];
+// create users
+
+
+
+// onlineUser.forEach(users=>{
+//     for (let user in users){
+//         fs.appendFile(path.join(__dirname,"main","online",`${users.name}.txt`),`\n${user}:${users[user]}`,(err)=>{
+//             if (err){
+//                 console.log(err);
+//                 throw err
+//             }
+//         })
+//     }
+// })
+// inPersonsUser.forEach(users=>{
+//     for (let user in users){
+//         fs.appendFile(path.join(__dirname,"main","inPersons",`${users.name}.txt`),`\n${user}:${users[user]}`,(err)=>{
+//             if (err){
+//                 console.log(err);
+//                 throw err
+//             }
+//         })
+//     }
+// })
+
+const substitute = () => {
+    fs.readFile(path.join(__dirname,"main","online","Andrii.txt"),(err, data1)=> {
+        if (err) {
+            console.log(err);
+            throw err
+        }
+        fs.readFile(path.join(__dirname, "main", "inPersons", "Oleg.txt"), (err, data2) => {
+            if (err) {
+                console.log(err);
+                throw err
+            }
+            fs.appendFile(path.join(__dirname, "main", "online", "Andrii.txt"), data2, {flag: "w"}, (err) => {
+                if (err) {
+                    console.log(err);
+                    throw err
+                }
+                fs.appendFile(path.join(__dirname, "main", "inPersons", "Oleg.txt"), data1, {flag: "w"}, (err) => {
+                    if (err) {
+                        console.log(err);
+                        throw err
+                    }
+
+                });
+
+            });
+
+        });
+    }) };
+substitute();
+
+
+
